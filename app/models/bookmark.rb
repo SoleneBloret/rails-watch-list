3988cd5d2b84f70575dfa78931cd01add2a4e5b1
Solene Bloret <solene.bloret@hotmail.fr>
2026-05-21 13:16:47 +0200

    models validations and associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  # Un signet doit être lié à un film et une liste,
+  # et les associations [film, liste] doivent être uniques
+  validates :comment, length: { minimum: 6,
+    too_short: "6 characters is the minimum required" }
+  validates :movie, uniqueness: { scope: :list }
 end
