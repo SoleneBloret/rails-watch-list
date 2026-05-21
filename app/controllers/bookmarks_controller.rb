6b845a9fa99c5022dfbf6a37d92a3e7a062d8bf0
Solene Bloret <solene.bloret@hotmail.fr>
2026-05-21 16:56:23 +0200

    show improvement with bookmark display

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,14 +7,17 @@ class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.save
-    # No need for app/views/restaurants/create.html.erb
-    redirect_to list_path(@list)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   # --------------- PRIVATE ---------------
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
